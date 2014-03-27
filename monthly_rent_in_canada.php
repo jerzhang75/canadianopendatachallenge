0bdd0179eb6a38b1f26b73cbc1c0735a158ec3d7
Jerry Zhang <zhangjtxx@hotmail.com>
2014-03-26 20:55:06 -0400

    Moved style to cssjpg

--- monthly_rent_in_canada.php
+++ monthly_rent_in_canada.php
@@ -4,6 +4,17 @@
   <script type="text/javascript" src="jquery.js"></script>
   <script type="text/javascript" src="nhpup_1.1.js"></script>
   <link href="style.css" rel="stylesheet" type="text/css">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42934369-4', 'net84.net');
+  ga('send', 'pageview');
+
+</script>
+
   <title>Monthly Rent Prices (Canada)</title>
   <h1>Monthly Rent in Canada</h1>
 
